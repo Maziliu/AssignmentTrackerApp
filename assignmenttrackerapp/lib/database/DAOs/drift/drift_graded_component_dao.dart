b0fcc053bdb2c988de6a50345b60a75c37dc698c
Martin Liu <144648079+Maziliu@users.noreply.github.com>
2025-02-15 02:12:49 -0700

    gradedCompoentn dao created

--- assignmenttrackerapp/lib/database/DAOs/drift/drift_graded_component_dao.dart
+++ assignmenttrackerapp/lib/database/DAOs/drift/drift_graded_component_dao.dart
@@ -8,4 +8,24 @@ part 'drift_graded_component_dao.g.dart';
 class DriftGradedComponentDao extends DatabaseAccessor<AppDatabase>
     with _$DriftGradedComponentDaoMixin {
   DriftGradedComponentDao(super.attachedDatabase);
+
+  Future<GradedComponent?> getGradedComponentById(int gradedComponentId) =>
+      (select(gradedComponents)
+            ..where((gradedComponent) =>
+                gradedComponent.id.equals(gradedComponentId)))
+          .getSingleOrNull();
+
+  Future<int> deleteGradedComponentById(int gradedComponentId) =>
+      (delete(gradedComponents)
+            ..where((gradedComponent) =>
+                gradedComponent.id.equals(gradedComponentId)))
+          .go();
+
+  Future<int> insertGradedComponentByCompanion(
+          GradedComponentsCompanion companion) =>
+      into(gradedComponents).insert(companion);
+
+  Future<bool> updateGradedComponentByCompanion(
+          GradedComponentsCompanion companion) =>
+      update(gradedComponents).replace(companion);
 }
