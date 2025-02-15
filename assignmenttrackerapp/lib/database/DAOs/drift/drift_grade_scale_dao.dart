bae44e647d167a12f20a3374b806efb3a33b47cc
Martin Liu <144648079+Maziliu@users.noreply.github.com>
2025-02-15 02:08:34 -0700

    gradeScale dao created

--- assignmenttrackerapp/lib/database/DAOs/drift/drift_grade_scale_dao.dart
+++ assignmenttrackerapp/lib/database/DAOs/drift/drift_grade_scale_dao.dart
@@ -8,4 +8,19 @@ part 'drift_grade_scale_dao.g.dart';
 class DriftGradeScaleDao extends DatabaseAccessor<AppDatabase>
     with _$DriftGradeScaleDaoMixin {
   DriftGradeScaleDao(super.attachedDatabase);
+
+  Future<GradeScale?> getGradeScaleById(int gradeScaleId) =>
+      (select(gradeScales)
+            ..where((gradeScale) => gradeScale.id.equals(gradeScaleId)))
+          .getSingleOrNull();
+
+  Future<int> deleteGradeScaleById(int gradeScaleId) => (delete(gradeScales)
+        ..where((gradeScale) => gradeScale.id.equals(gradeScaleId)))
+      .go();
+
+  Future<int> insertGradeScaleByCompanion(GradeScalesCompanion companion) =>
+      into(gradeScales).insert(companion);
+
+  Future<bool> updateGradeScaleByCompanion(GradeScalesCompanion companion) =>
+      update(gradeScales).replace(companion);
 }
