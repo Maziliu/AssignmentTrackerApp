b1cc07724cf9d3ff41988becf4787c45865606cc
Martin Liu <144648079+Maziliu@users.noreply.github.com>
2025-02-16 18:19:18 -0700

    DriftAssessmentRepo created

--- assignmenttrackerapp/lib/models/app_model_assessment.dart
+++ assignmenttrackerapp/lib/models/app_model_assessment.dart
@@ -10,7 +10,7 @@ class AppModelAssessment extends AppModel implements Datastreamable {
   final String _userId, _assessmentName;
   final AssessmentType _assessmentType;
   final bool _isComplete;
-  final int _gradedComponentId, _eventId;
+  final int _gradedComponentId, _eventId, _courseId;
 
   AppModelAssessment(
       {required super.id,
@@ -19,19 +19,22 @@ class AppModelAssessment extends AppModel implements Datastreamable {
       required AssessmentType assessmentType,
       required bool isComplete,
       required int gradedComponentId,
-      required int eventId})
+      required int eventId,
+      required int courseId})
       : _userId = userId,
         _assessmentName = assessmentName,
         _assessmentType = assessmentType,
         _isComplete = isComplete,
         _gradedComponentId = gradedComponentId,
-        _eventId = eventId;
+        _eventId = eventId,
+        _courseId = courseId;
 
   String get assessmentName => _assessmentName;
   AssessmentType get assessmentType => _assessmentType;
   bool get isComplete => _isComplete;
   int get gradedComponentId => _gradedComponentId;
   int get eventId => _eventId;
+  int get courseId => _courseId;
 
   @override
   String get ownerId => _userId;
